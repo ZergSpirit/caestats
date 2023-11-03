be19a118b488d65283c54cb22ff56537e72d13c2
ORSIER <adrien.orsier@ars.sante.fr>
2023-11-03 12:10:48 +0100

    Ajout d'une page avec le résultat des tournois

--- src/Controller/ZitsDetailController.php
+++ src/Controller/ZitsDetailController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Joueur;
 use App\Entity\Rank;
+use App\Entity\Tournoi;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,8 +14,8 @@ class ZitsDetailController extends AbstractController
 {
     public function __construct(private EntityManagerInterface $entityManager){}
 
-    #[Route('/zits/detail/{joueurId}', name: 'app_zits_detail')]
-    public function index(?int $joueurId): Response
+    #[Route('/zits/detail/joueur/{joueurId}', name: 'app_zits_joueur_detail')]
+    public function joueurDetail(?int $joueurId): Response
     {
 
         $joueur = $this->entityManager->getRepository(Joueur::class)->find($joueurId);
@@ -22,11 +23,21 @@ class ZitsDetailController extends AbstractController
         $archiveRanks = $this->entityManager->getRepository(Rank::class)->findAllArchivedRanks($joueur);
 
 
-        return $this->render('zits_detail/index.html.twig', [
+        return $this->render('zits_detail/joueur.html.twig', [
             'controller_name' => 'ZitsDetailController',
             'joueur' => $joueur,
             'ranks' => $ranks,
             'archivedRanks' => $archiveRanks
+        ]);
+    }
+
+    #[Route('/zits/detail/tournoi/{tournoiId}', name: 'app_zits_tournoi_detail')]
+    public function tournoiDetail(?int $tournoiId): Response
+    {
+        $tournoi = $this->entityManager->getRepository(Tournoi::class)->find($tournoiId);
+        return $this->render('zits_detail/tournoi.html.twig', [
+            'controller_name' => 'ZitsDetailController',
+            'tournoi' => $tournoi
         ]);
     }
 }
